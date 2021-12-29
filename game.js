4f7c9305385bdab6af68d3a3b36627e5142ff943
Ömer Faruk Özbek <omer.ozbek.faruk@gmail.com>
2021-12-30 00:30:30 +0300

    First release

---
+++ game.js
@@ -0,0 +1,94 @@
+// Initialize the first variables.
+var gamePattern = [];
+var userClickedPattern = [];
+var gameStatus = false;
+var level = 0;
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+// Controls that any keyboard events happened.
+$(document).keydown(function(){
+  if(gameStatus === false){
+    $('h1').text("Level " + level);
+    nextSequence();
+    gameStatus = true;
+  }
+});
+
+// Start Button click control.
+$('.start-button').click(function(){
+  if(gameStatus === false){
+    $('h1').text("Level " + level);
+    nextSequence();
+    gameStatus = true;
+  }
+});
+
+// Make a new sequence
+function nextSequence() {
+  level++;
+  $('h1').text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  //Select button and make a flash
+  $("#" + randomChosenColour).css({opacity: 0});
+  $("#" + randomChosenColour).animate({opacity: 1}, 700);
+
+  playSound(randomChosenColour);
+}
+
+//Play the audio
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+//Button click handler
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  animatePress(userChosenColour);
+  playSound(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+//Animate the clicked buttons
+function animatePress(currentColour){
+  $('.'+ currentColour).addClass("pressed")
+  setTimeout(function(){
+    $('.'+ currentColour).removeClass("pressed");
+  },100);
+}
+
+// Check the patterns
+function checkAnswer(currentLevel){
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+    //console.log("success");
+    if(userClickedPattern.length === gamePattern.length){
+      setTimeout(function(){
+        nextSequence();
+      },1000);
+    }
+  }
+  else{
+    //console.log("wrong");
+    playSound('wrong');
+    $('body').addClass('game-over');
+    setTimeout(function(){
+      $('body').removeClass('game-over');
+    },200);
+    $('h1').text('Game Over, Press Any Key to Restart');
+    startOver();
+  }
+}
+
+//Restart the game
+function startOver(){
+  gamePattern = [];
+  gameStatus = false;
+  level = 0;
+  userClickedPattern = [];
+}
